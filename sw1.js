52d4adc1cd316aa32fa641308b4ba00ad7d351cd
gaboritaly <33197105+gaboritaly@users.noreply.github.com>
2023-06-27 15:00:09 +0200

    Update sw1.js

--- sw1.js
+++ sw1.js
@@ -5,7 +5,7 @@
 
 self.addEventListener('push', (event) => {
   const data = event.data.json();
-  console.log(event.data)
+  console.log(data)
   event.waitUntil(
     self.registration.showNotification(data.title, data.options )
   );
