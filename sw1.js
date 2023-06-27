0b1a7aa5ca2553228fb78cea1195bc8728567842
gaboritaly <33197105+gaboritaly@users.noreply.github.com>
2023-06-27 12:21:54 +0200

    Update sw1.js

--- sw1.js
+++ sw1.js
@@ -1,3 +1,12 @@
-self.addEventListener('push', (options) => {
-  self.registration.showNotification('Hello world!');
+/* self.addEventListener('push', (options) => {
+  self.registration.showNotification('Hello world!',options);
+});
+ */
+
+self.addEventListener('push', (event) => {
+  event.waitUntil(
+    // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+    // as the body.
+    self.registration.showNotification('ServiceWorker Cookbook', event.data)
+  );
 });
